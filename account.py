ae2ecbd0f63897362041c338c4137598d411c60d
Gopalasetti-Hamalata <gopalasetti.hamalata@hcltech.com>
2026-05-02 19:42:12 +0530

    account.py

--- account.py
+++ account.py
@@ -1,27 +1,15 @@
 class Account:
-
     def __init__(self, pin, balance=10000):
-
         self.pin = pin
-
         self.balance = balance
-
-
+ 
     def check_pin(self, entered_pin):
-
         return self.pin == entered_pin
-
-
+ 
     def withdraw(self, amount):
-
         if amount <= 0:
-
             return "Invalid amount"
-
         if amount > self.balance:
-
             return "Insufficient balance"
-
         self.balance -= amount
-
         return f"Withdrawn ₹{amount}"
